cbc7950a55c7a89272d398e760849d7b8c447fa9
lzs95 <86816779+lzs95@users.noreply.github.com>
2022-05-16 19:00:55 +0900

    feat: home button and url bar

--- main/app.py
+++ main/app.py
@@ -1,4 +1,3 @@
-from imp import reload
 import sys
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import * 
@@ -8,12 +7,14 @@ from PyQt5.QtWebEngineWidgets import *
 # Main Window Handles Graphics and Logic
 class MainWindow(QMainWindow):
     def __init__(self):
+        
         super(MainWindow, self).__init__()
         self.browser = QWebEngineView()
         self.browser.setUrl(QUrl("https://duckduckgo.com/"))
         self.setCentralWidget(self.browser)
         super().showMaximized()
-        
+    
+
         #Navbar
         navbar = QToolBar()
         self.addToolBar(navbar)  
@@ -30,11 +31,34 @@ class MainWindow(QMainWindow):
         reload_button = QAction('Reload',self)
         reload_button.triggered.connect(self.browser.reload)
         navbar.addAction(reload_button)
-    
+        #Home Button
+        home_button = QAction("Home", self)
+        home_button.triggered.connect(self.navigate_home)
+        navbar.addAction(home_button)
+        #Url_Bar
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        navbar.addWidget(self.url_bar)
         
+        #Capture URL Chain
+        self.browser.urlChanged.connect(self.url_update)
+        
+    #Home button returns to specified Website    
+    def navigate_home(self):
+        self.browser.setUrl(QUrl('https://duckduckgo.com/'))
+    
+    def navigate_to_url(self):
+        #Gets input from url_bar
+        url = self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+        
+    def url_update(self, url):
+        self.url_bar.setText(url.toString())
+        
+    
      
 # QApplication Handles Events and Initialisation   
 app = QApplication(sys.argv)
-QApplication.setApplicationName('My Browser')
+QApplication.setApplicationName('Muh Chrome')
 window = MainWindow()
 app.exec_()      
